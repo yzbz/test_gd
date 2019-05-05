70c1902e6782817567e7872b5576e250ae0bf7f5
zhangpeng <523631013@qq.com>
2019-05-05 11:06:48 +0800

    edit helper

--- helper/helper.php
+++ helper/helper.php
@@ -5,25 +5,23 @@ if(IS_CLI){
     ]);
 }
 
-$fontFileName = 'fzhtjt.ttf';
 $demoDir = ROOT_PATH . "gd_demo";
-$fontFile = $demoDir . '/' . $fontFileName;
-$sourceFontFile = __DIR__ . '/' . $fontFileName;
 
 $extendFiles = [
+    'fzhtjt.ttf',
     'bg.png',
 ]; //当前目录下使用的文件
 
-if (!file_exists($fontFile)) {
-    if (file_exists($sourceFontFile)) {
+if (!file_exists($demoDir . '/' . $extendFiles[0])) {
+    if (file_exists(__DIR__ . '/' . $extendFiles[0])) {
         if (!is_dir($demoDir)) {
             mkdir($demoDir, 0777, true);
         }
-        copy($sourceFontFile, $fontFile);
         foreach ($extendFiles as $item) {
             copy(__DIR__ . '/' . $item, $demoDir . '/' . $item);
         }
     } else {
-        echo "源字体文件" . $sourceFontFile . "不存在";
+        echo "源字体文件" . $demoDir . '/' . $extendFiles[0] . "不存在";
     }
 }
+
