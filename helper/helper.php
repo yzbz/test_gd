5a2fe2f54dce0f6bd79a8938143a822fccb90f8f
zhangpeng <523631013@qq.com>
2019-05-05 10:45:36 +0800

    增加字体文件和背景图等

--- helper/helper.php
+++ helper/helper.php
@@ -4,3 +4,26 @@ if(IS_CLI){
         "\\zhangpeng\\gd\\MakeDemo"
     ]);
 }
+
+$fontFileName = 'fzhtjt.ttf';
+$demoDir = ROOT_PATH . "gd_demo";
+$fontFile = $demoDir . '/' . $fontFileName;
+$sourceFontFile = __DIR__ . '/' . $fontFileName;
+
+$extendFiles = [
+    'bg.png',
+]; //当前目录下使用的文件
+
+if (!file_exists($fontFile)) {
+    if (file_exists($sourceFontFile)) {
+        if (!is_dir($demoDir)) {
+            mkdir($demoDir, 0777, true);
+        }
+        copy($sourceFontFile, $fontFile);
+        foreach ($extendFiles as $item) {
+            copy(__DIR__ . '/' . $item, $demoDir . '/' . $item);
+        }
+    } else {
+        echo "源字体文件" . $sourceFontFile . "不存在";
+    }
+}
